f1564c06e5ec680bac8ed5e5dbe812623cfba11f
Turkey <turkey2349@gmail.com>
2015-07-20 00:19:04 -0400

    - Updated to version 1.2 - Mystical Ore now scales with the fortune that is on the pickaxe! 10% higher chance to get each drop per level of fortune -  Hostile mobs should now spawn. Hopefully...... Muwahahahaha

--- src/main/java/com/turkey/turkeyUtil/Blocks/MysticalOreBlock.java
+++ src/main/java/com/turkey/turkeyUtil/Blocks/MysticalOreBlock.java
@@ -15,8 +15,8 @@ import com.turkey.turkeyUtil.UtilAchievements;
 import com.turkey.turkeyUtil.Items.UtilItems;
 
 public class MysticalOreBlock extends Block
-{	
-	public MysticalOreBlock() 
+{
+	public MysticalOreBlock()
 	{
 		super(Material.ground);
 		setHardness(.3f);
@@ -24,37 +24,39 @@ public class MysticalOreBlock extends Block
 		setBlockName("Mystical_Ore_Block");
 		setCreativeTab(TurkeyUtil.baseModTab);
 	}
-	
+
 	@Override
 	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
 	{
+		int fortuneAdd = fortune * 10;
+		System.out.println(fortuneAdd);
 		Random r = new Random();
 		ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-		if(r.nextInt(100) < 60)
-			drops.add(new ItemStack(Items.coal, r.nextInt(10) + 1));
-		if(r.nextInt(100) < 15)
-			drops.add(new ItemStack(UtilItems.fabulousDiamond, r.nextInt(3) + 1));
-		if(r.nextInt(100) < 20)
-			drops.add(new ItemStack(Items.quartz, r.nextInt(3) + 1));
-		if(r.nextInt(100) < 35)
-			drops.add(new ItemStack(Items.dye, r.nextInt(7) + 1, 4));
-		if(r.nextInt(100) < 35)
-			drops.add(new ItemStack(Items.brick, r.nextInt(7) + 1));
-		if(r.nextInt(100) < 35)
-			drops.add(new ItemStack(Items.redstone, r.nextInt(7) + 1));
-		if(r.nextInt(100) < 30)
-			drops.add(new ItemStack(Items.iron_ingot, r.nextInt(7) + 1));
-		if(r.nextInt(100) < 25)
-			drops.add(new ItemStack(Items.gold_ingot, r.nextInt(3) + 1));
-		if(r.nextInt(100) < 15)
-			drops.add(new ItemStack(Items.emerald, r.nextInt(3) + 1));
-		if(r.nextInt(100) < 15)
-			drops.add(new ItemStack(Items.diamond, r.nextInt(3) + 1));
-		
+		if(r.nextInt(100) < (60 + fortuneAdd))
+			drops.add(new ItemStack(Items.coal, r.nextInt(10) + 1 + fortune));
+		if(r.nextInt(100) < (15 + fortuneAdd))
+			drops.add(new ItemStack(UtilItems.fabulousDiamond, r.nextInt(3) + 1 + fortune));
+		if(r.nextInt(100) < (20 + fortuneAdd))
+			drops.add(new ItemStack(Items.quartz, r.nextInt(3) + 1 + fortune));
+		if(r.nextInt(100) < (35 + fortuneAdd))
+			drops.add(new ItemStack(Items.dye, r.nextInt(7) + 1 + fortune, 4));
+		if(r.nextInt(100) < (35 + fortuneAdd))
+			drops.add(new ItemStack(Items.brick, r.nextInt(7) + 1 + fortune));
+		if(r.nextInt(100) < (35 + fortuneAdd))
+			drops.add(new ItemStack(Items.redstone, r.nextInt(7) + 1 + fortune));
+		if(r.nextInt(100) < (30 + fortuneAdd))
+			drops.add(new ItemStack(Items.iron_ingot, r.nextInt(7) + 1 + fortune));
+		if(r.nextInt(100) < (25 + fortuneAdd))
+			drops.add(new ItemStack(Items.gold_ingot, r.nextInt(3) + 1 + fortune));
+		if(r.nextInt(100) < (15 + fortuneAdd))
+			drops.add(new ItemStack(Items.emerald, r.nextInt(3) + 1 + fortune));
+		if(r.nextInt(100) < (15 + fortuneAdd))
+			drops.add(new ItemStack(Items.diamond, r.nextInt(3) + 1 + fortune));
+
 		return drops;
 	}
-	
-	public void onBlockHarvested(World world, int x, int y, int z, int meta, EntityPlayer player) 
+
+	public void onBlockHarvested(World world, int x, int y, int z, int meta, EntityPlayer player)
 	{
 		player.addStat(UtilAchievements.mysticalOre, 1);
 	}
--- src/main/java/com/turkey/turkeyUtil/Mobs/UtilMobs.java
+++ src/main/java/com/turkey/turkeyUtil/Mobs/UtilMobs.java
@@ -8,20 +8,19 @@ import com.turkey.turkeyUtil.TurkeyUtil;
 
 import cpw.mods.fml.common.registry.EntityRegistry;
 
-public class UtilMobs 
+public class UtilMobs
 {
 
-	
 	public static void loadMobs()
 	{
-		createEntity(EntityTurkey.class, "Turkey" , 0x522900, 0xE68A00);
-		createEntity(EntityDuck.class, "Duck" , 0xA65300, 0x005300);
-		createEntity(EntityRhea.class, "Rhea" , 0x291400, 0x694016);
-		createEntity(EntityVoxel.class, "Voxel" , 0x999999, 0xEBEBEB);
-		//createHostileEntity(EntityPoisonSkeleton.class, "Poison_Skeleton" , 0xCCCCCC, 0x009933);
-		createHostileEntity(EntityDamageSkeleton.class, "Damage_Skeleton" , 0xCCCCCC, 0x800000);
+		createEntity(EntityTurkey.class, "Turkey", 0x522900, 0xE68A00);
+		createEntity(EntityDuck.class, "Duck", 0xA65300, 0x005300);
+		createEntity(EntityRhea.class, "Rhea", 0x291400, 0x694016);
+		createEntity(EntityVoxel.class, "Voxel", 0x999999, 0xEBEBEB);
+		createHostileEntity(EntityPoisonSkeleton.class, "Poison_Skeleton", 0xCCCCCC, 0x009933);
+		createHostileEntity(EntityDamageSkeleton.class, "Damage_Skeleton", 0xCCCCCC, 0x800000);
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void createEntity(Class entityClass, String entityName, int solidColor, int spotColor)
 	{
@@ -29,18 +28,24 @@ public class UtilMobs
 		EntityRegistry.registerGlobalEntityID(entityClass, entityName, id);
 		EntityRegistry.registerModEntity(entityClass, entityName, id, TurkeyUtil.instance, 64, 1, true);
 		EntityRegistry.addSpawn(entityClass, 5, 5, 5, EnumCreatureType.creature, BiomeGenBase.forest, BiomeGenBase.birchForest, BiomeGenBase.birchForestHills, BiomeGenBase.forestHills, BiomeGenBase.roofedForest);
-		
+
 		EntityList.entityEggs.put(id, new EntityList.EntityEggInfo(id, solidColor, spotColor));
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void createHostileEntity(Class entityClass, String entityName, int solidColor, int spotColor)
 	{
 		int id = EntityRegistry.findGlobalUniqueEntityId();
 		EntityRegistry.registerGlobalEntityID(entityClass, entityName, id);
 		EntityRegistry.registerModEntity(entityClass, entityName, id, TurkeyUtil.instance, 64, 1, true);
-		EntityRegistry.addSpawn(entityClass, 10, 5, 5, EnumCreatureType.monster);
-		
 		EntityList.entityEggs.put(id, new EntityList.EntityEggInfo(id, solidColor, spotColor));
+
+		for(int i = 0; i < BiomeGenBase.getBiomeGenArray().length; i++)
+		{
+			if(BiomeGenBase.getBiomeGenArray()[i] != null)
+			{
+				EntityRegistry.addSpawn(entityClass, 10, 5, 5, EnumCreatureType.monster, BiomeGenBase.getBiomeGenArray()[i]);
+			}
+		}
 	}
 }
